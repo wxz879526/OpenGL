3ab2aaeb9c9abc64ca47ba32ee93f4b193e9b587
wenxinzhou <wenxinzhou@areasleader.com>
2018-09-02 19:49:20 +0800

    shader

--- Tutorial/Demo/Demo/main.cpp
+++ Tutorial/Demo/Demo/main.cpp
@@ -1,10 +1,8 @@
-#define GLEW_STATIC
-#include <GL/glew.h>
 
-
-#include <GLFW/glfw3.h>
-
+#include "Shader.h"
 #include <iostream>
+
+const GLuint WIDTH = 800, HEIGHT = 600;
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
 {
@@ -14,14 +12,16 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 
 int main()
 {
+	std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	GLFWwindow *window = glfwCreateWindow(800,
-		600, "Learn OpenGL", nullptr, nullptr);
+	GLFWwindow *window = glfwCreateWindow(WIDTH,
+		HEIGHT, "Learn OpenGL", nullptr, nullptr);
 	if (nullptr == window)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -30,6 +30,7 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetKeyCallback(window, key_callback);
+
 
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK)
@@ -42,13 +43,56 @@ int main()
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
 
+	GLfloat vertices[] = {
+		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+	};
+
+	GLuint VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof
+		GLfloat));
+	glEnableVertexAttribArray(1);
+
+	Shader shader("shader.vs", "shader.frag");
+
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
-		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+		/*glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);*/
+
+		shader.Use();
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
 		glfwSwapBuffers(window);
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 
 	glfwTerminate();
 
